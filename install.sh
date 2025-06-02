be29c7fa7a36ec00f034017afbc019c1d731b62e
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-06-02 17:38:11 +0200

    removing install of python packages

--- install.sh
+++ install.sh
@@ -3,15 +3,6 @@
 SCRIPT_DIR=$(dirname "$0")
 cp -r ${SCRIPT_DIR}/.config/jasspa ~/.config/
 /bin/sh -c "$(curl -fsSL https://github.com/bjasspa/jasspa/releases/latest/download/microemacs-install)"
-if [[ `which cppcheck` == "" ]]; then 
-    pip3 install cppcheck --user
-fi
-if [[ `which cpplint` == "" ]]; then 
-    pip3 install cpplint --user
-fi
-if [[ `which astyle` == "" ]]; then 
-    pip3 install astyle --user
-fi
 
 function install-fonts {
     if [[ -z $1 ]]; then
@@ -20,18 +11,22 @@ function install-fonts {
         DIR=$1
     fi
     [[ -d $DIR ]] || mkdir -p $DIR
+    echo "linking local fonts into $DIR"
     find /usr -iname "*Mono*.ttf" 2>/dev/null | xargs ln -sf -t ${DIR}
     if [[ ! -f ${DIR}/ubuntu-mono-bold.ttf ]]; then
+        echo "installing Ubuntu fonts ..."
         wget -q https://github.com/braver/programmingfonts/raw/gh-pages/fonts/resources/ubuntu/ubuntu-bold.ttf -O ${DIR}/ubuntu-mono-bold.ttf
         wget -q https://github.com/braver/programmingfonts/raw/gh-pages/fonts/resources/ubuntu/ubuntu.ttf    -O ${DIR}/ubuntu-mono-medium.ttf    
         wget -q https://github.com/braver/programmingfonts/raw/gh-pages/fonts/resources/ubuntu/license.txt -O ${DIR}/ubuntu-license.txt    
     fi  
     if [[ ! -f ${DIR}/source-code-pro-medium.ttf ]]; then 
+        echo "installing source code pro fonts ..."
         wget -q https://github.com/adobe-fonts/source-code-pro/raw/release/LICENSE.md -O ${DIR}/source-code-pro-license.txt
 	wget -q https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Bold.ttf -O ${DIR}/source-code-pro-bold.ttf
 	wget -q https://github.com/adobe-fonts/source-code-pro/raw/release/TTF/SourceCodePro-Regular.ttf -O ${DIR}/source-code-pro-medium.ttf     
     fi	
     if [[ ! -f ${DIR}/courier-prime-medium.ttf ]]; then 
+        echo "installing courier prime fonts ..."
         wget -q https://github.com/quoteunquoteapps/CourierPrime/raw/master/fonts/ttf/CourierPrime-Regular.ttf -O ${DIR}/courier-prime-medium.ttf
 	wget -q https://github.com/quoteunquoteapps/CourierPrime/raw/master/fonts/ttf/CourierPrime-Bold.ttf -O ${DIR}/courier-prime-bold.ttf
 	wget -q https://github.com/quoteunquoteapps/CourierPrime/blob/master/OFL.txt -O ${DIR}/courier-prime-license.txt
