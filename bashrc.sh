677431674faaae23ee1cee4037fa4e03ba1926ec
Detlef Groth <75636+mittelmark@users.noreply.github.com>
2025-06-03 18:17:59 +0200

    adding backup code

--- bashrc.sh
+++ bashrc.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 alias mfontsel="xfontsel -pattern '*-m-*' -scale"
 export PATH=~/.local/bin:$PATH
+function backup-cpp {
+    cd ~
+    zip -yr .config/jasspa .local/bin .local/share/jasspa \
+        .local/share/fonts ./PEX-labs
+}
+        
+    
